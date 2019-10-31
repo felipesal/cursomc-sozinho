65f9b6cfaba12ad7dde749144ea45706e288b538
Felipe Salles <prof.felipe.salles@gmail.com>
2019-10-31 15:43:15 -0300

    Primeira classe de dominio: Categoria

--- src/main/java/com/example/cursomcsozinho/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomcsozinho/resources/CategoriaResource.java
@@ -1,14 +1,26 @@
 package com.example.cursomcsozinho.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cursomcsozinho.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		List<Categoria> categorias = new ArrayList<>();
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categorias.addAll(Arrays.asList(cat1, cat2));		
+		return categorias;
 	}
 }
