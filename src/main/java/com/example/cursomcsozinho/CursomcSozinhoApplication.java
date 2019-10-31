45ccc9a1bfd9ca7d23fb783927fd0fb97e4a185c
Felipe Salles <prof.felipe.salles@gmail.com>
2019-10-31 15:13:39 -0300

    Projeto criado

---
+++ src/main/java/com/example/cursomcsozinho/CursomcSozinhoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursomcsozinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcSozinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcSozinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cursomcsozinho/CursomcSozinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursomcsozinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcSozinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
