e28d2c2d092aa73dd08257585f1ccda1d590a162
Felipe Salles <prof.felipe.salles@gmail.com>
2019-11-02 00:55:05 -0300

    Classes cidade e estado + mapeamento

---
+++ src/main/java/com/example/cursomcsozinho/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.example.cursomcsozinho.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.cursomcsozinho.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+	
+	
+}
---
+++ src/main/java/com/example/cursomcsozinho/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.example.cursomcsozinho.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.cursomcsozinho.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
