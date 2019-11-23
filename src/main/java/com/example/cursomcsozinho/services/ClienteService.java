beab781eabaaff5c57f80090026cc1833e89a4cd
Felipe Salles <prof.felipe.salles@gmail.com>
2019-11-23 03:31:44 -0200

    Put, Delete e get para cliente

--- src/main/java/com/example/cursomcsozinho/services/ClienteService.java
+++ src/main/java/com/example/cursomcsozinho/services/ClienteService.java
@@ -1,13 +1,19 @@
 package com.example.cursomcsozinho.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import com.example.cursomcsozinho.domain.Categoria;
 import com.example.cursomcsozinho.domain.Cliente;
+import com.example.cursomcsozinho.domain.dto.ClienteDTO;
 import com.example.cursomcsozinho.repositories.ClienteRepository;
+import com.example.cursomcsozinho.resources.exceptions.DataIntegrityException;
 import com.example.cursomcsozinho.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,5 +28,44 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
+	}
+	
+	public void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível deletar pois existem entidade associadas.");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Cliente fromDto(ClienteDTO clienteDto) {
+		return new Cliente(clienteDto.getId(), clienteDto.getNome(), clienteDto.getEmail(), null, null);
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
 	}
 }
