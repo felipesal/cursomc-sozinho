d96a0122901b4cf22984eb83471ffd1fb78457a8
Felipe Salles <prof.felipe.salles@gmail.com>
2019-11-26 02:10:54 -0200

    Inserindo novo pedido

--- src/main/java/com/example/cursomcsozinho/domain/PagamentoComCartao.java
+++ src/main/java/com/example/cursomcsozinho/domain/PagamentoComCartao.java
@@ -3,7 +3,9 @@ package com.example.cursomcsozinho.domain;
 import javax.persistence.Entity;
 
 import com.example.cursomcsozinho.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
