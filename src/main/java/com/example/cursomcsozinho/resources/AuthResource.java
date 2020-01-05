98a1ec9ec96caa95cb7115b12a09463fd5ed81da
Felipe Salles <prof.felipe.salles@gmail.com>
2020-01-05 02:05:27 -0200

    Gerando refresh token

--- src/main/java/com/example/cursomcsozinho/resources/AuthResource.java
+++ src/main/java/com/example/cursomcsozinho/resources/AuthResource.java
@@ -1,5 +1,30 @@
 package com.example.cursomcsozinho.resources;
 
-public class AuthResource {
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cursomcsozinho.security.JWTUtil;
+import com.example.cursomcsozinho.security.UserSS;
+import com.example.cursomcsozinho.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+	UserSS user = UserService.authenticated();
+	String token = jwtUtil.generateToken(user.getUsername());
+	response.addHeader("Authorization", "Bearer " + token);
+	return ResponseEntity.noContent().build();
+	}
+	
 }
