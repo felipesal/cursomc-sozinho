db7279f5962cf12b881f7007faade07be467d302
Felipe Salles <prof.felipe.salles@gmail.com>
2020-01-22 01:20:13 -0300

    Inserindo imagem via endpoint

--- src/main/java/com/example/cursomcsozinho/CursomcSozinhoApplication.java
+++ src/main/java/com/example/cursomcsozinho/CursomcSozinhoApplication.java
@@ -1,17 +1,12 @@
 package com.example.cursomcsozinho;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.cursomcsozinho.services.S3Service;
-
 @SpringBootApplication
 public class CursomcSozinhoApplication implements CommandLineRunner {
 	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcSozinhoApplication.class, args);
@@ -19,7 +14,7 @@ public class CursomcSozinhoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\Livia\\Desktop\\AWS\\ana.jpg");
+	
 	
 	}
 
